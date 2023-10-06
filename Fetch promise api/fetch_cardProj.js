0070d8552bff8c995c87880642daa74e08367c50
Dhaval <joshidhaval2002@gmail.com>
2023-10-06 23:39:47 +0530

    Promise chaining in Javascript

--- Fetch promise api/fetch_cardProj.js
+++ Fetch promise api/fetch_cardProj.js
@@ -1,22 +1,27 @@
 const divEle = document.querySelector('.card-container');
-function getDetails(id){
+function getDetails(id) {
 
     //The fetch() api return a Promise
-    const request=fetch(`https://dummyjson.com/users/${id}`)  //return a promise object 
-    console.log(request)
-    
-    const response=request.then((response)=> {
-        
-        console.log(response) //this gives the response object which is inside promise object in this[[PromiseResult]]  
+    // const request = fetch(`https://dummyjson.com/users/${id}`)  //return a promise object 
+    // console.log(request)
 
-         //console.log(response.json())  // this will give the data in Javascript Object it is stored in[[PromiseResult]] : Object
-          //Since json() is return a promise object we can use .then to access the result
+    // const response = request.then((response) => {
 
-          return response.json()  //.then is callback function it return
-    })
+    //     console.log(response) //this gives the response object which is inside promise object in this[[PromiseResult]]  
 
-    response.then((value)=>console.log(value))//this will give the Object form the above then if we return it
-    
+    //     //console.log(response.json())  // this will give the data in Javascript Object it is stored in[[PromiseResult]] : Object
+    //     //Since json() is return a promise object we can use .then to access the result
+
+    //     return response.json()  //.then is callback function it return
+    // })
+
+    // response.then((value) => console.log(value))//this will give the Object form the above then if we return it
+
+    //The above code can be wriiten in the form as mentioned BELOW .This is called Promise chaining
+    fetch(`https://dummyjson.com/users/${id}`)
+    .then((response) => {
+        return response.json()  //.then is callback function it return
+    }).then((value) => console.log(value)) //THis will give output as Javascript object
 
 }
 
