3084dd0d11ae399b61579b8384a34a9ad84f9ba0
Dhaval <joshidhaval2002@gmail.com>
2023-10-06 22:59:18 +0530

    .then is callback function return JS object

--- Fetch promise api/fetch_cardProj.js
+++ Fetch promise api/fetch_cardProj.js
@@ -6,13 +6,17 @@ function getDetails(id){
     console.log(request)
     
     const response=request.then((response)=> {
+        
         console.log(response) //this gives the response object which is inside promise object in this[[PromiseResult]]  
 
-         console.log(response.json())  // this will give the data in Javascript Object it is stored in[[PromiseResult]] : Object
+         //console.log(response.json())  // this will give the data in Javascript Object it is stored in[[PromiseResult]] : Object
           //Since json() is return a promise object we can use .then to access the result
+
+          return response.json()  //.then is callback function it return
     })
 
-    response.then((value)=>console.log(value))
+    response.then((value)=>console.log(value))//this will give the Object form the above then if we return it
+    
 
 }
 
