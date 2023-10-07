121c23c85fc1dc31b78ab606f61bc45b2f157c3d
Dhaval <joshidhaval2002@gmail.com>
2023-10-07 14:36:25 +0530

    corrected code

--- Fetch promise api/fetch_cardProj.js
+++ Fetch promise api/fetch_cardProj.js
@@ -19,20 +19,21 @@ function getDetails(id) {
 
     //The above code can be wriiten in the form as mentioned BELOW .This is called Promise chaining
     fetch(`https://dummyjson.com/users/${id}`)
-    .then((response) => {
+        .then((response) => {
 
-        if(!response.ok){ //if the response object dont contain the data of the required ID ok:will be False
-            //with the help of throw we can write the custom error message and using Error(construtor)
-            throw new Error('This message is inside the error') //the throw works as a return keyword
-        }
+            console.log(response)// for debugging to check if id is 0 there will be no response from serber and status ok is false.
+            if (!response.ok) { //if the response object dont contain the data of the required ID ok:will be False.
+                //with the help of throw we can write the custom error message and using Error(construtor)
+                throw new Error('This message is inside the error') //the throw works as a return keyword
+            }
 
-        return response.json()  //.then is callback function it return
-    }).then((value) => console.log(value)) //THis will give output as Javascript object
-    
-    //The message in trhow will be catch by below "NOTE:IF NOT USED THROW for CUSTOM ERROR MESSAGE IT WILL GIVE BY DEFAULT ERROR MESSAGE"
-    .catch((err)=>{
-        console.log(err)   //this will give the error message in normal form like a text message
-        console.error(err)  //this will give the message in red like an error in console
-    })
+            return response.json()  //.then is callback function it return
+        }).then((value) => console.log(value)) //THis will give output as Javascript object
+
+        //The message in trhow will be catch by below "NOTE:IF NOT USED THROW for CUSTOM ERROR MESSAGE IT WILL GIVE BY DEFAULT ERROR MESSAGE"
+        .catch((err) => {
+            console.log(err)   //this will give the error message in normal form like a text message
+            console.error(err)  //this will give the message in red like an error in console
+        })
 }
 getDetails(0)
